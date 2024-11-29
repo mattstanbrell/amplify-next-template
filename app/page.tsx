6b404ea9601fc366dd5d81e8a62e39da005b463d
Matt Stanbrell <mattstanbrell@gmail.com>
2024-11-29 21:09:34 +0000

    move amplify_outputs.json

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
 "use client";
 
-import { useState, useEffect } from "react";
-import { generateClient } from "aws-amplify/data";
 import type { Schema } from "@/amplify/data/resource";
+import { generateClient } from "aws-amplify/data";
+import { useEffect, useState } from "react";
 import "./../app/app.css";
 import { Amplify } from "aws-amplify";
-import outputs from "@/amplify_outputs.json";
+import outputs from "../amplify_outputs.json";
 import "@aws-amplify/ui-react/styles.css";
 
 Amplify.configure(outputs);
